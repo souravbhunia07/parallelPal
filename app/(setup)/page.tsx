8c6a11455380763308e808b222c8396abf4e2f31
Sourav Kumar Bhunia <souravbhunia400@gmail.com>
2023-10-01 20:54:57 +0530

    getting error in message upload

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,29 @@
+import { redirect } from "next/navigation";
+import { intialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { InitialModal } from "@/components/models/initial-model";
+
+const SetupPage = async () => {
+
+    const profile = await intialProfile();
+
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if(server) {
+        return redirect(`/servers/${server.id}`);
+    }
+
+    return ( 
+        <InitialModal />
+     );
+}
+ 
+export default SetupPage;
---
+++ components/file-upload.tsx
@@ -0,0 +1,47 @@
+"use Client";
+
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+    onChange: (url?: string) => void;
+    value: string;
+    endpoint: "messageFile" | "serverImage"
+}
+
+export const FileUpload = ({
+    onChange,
+    value,
+    endpoint
+}: FileUploadProps) => {
+
+    const fileType = value?.split(".").pop();
+
+    if(value && fileType !== "pdf") {
+        return (
+            <div className="relative h-20 w-20">
+                <Image 
+                    fill
+                    src={value}
+                    alt="Upload"
+                    className="rounded-full"
+                />
+            </div>
+        )
+    }
+
+    return (
+        <UploadDropzone 
+            endpoint={endpoint}
+            onClientUploadComplete={(res) => {
+                onChange(res?.[0].url);
+            }}
+            onUploadError={(error: Error) => {
+                console.log(error);
+            }}
+        />
+    )
+}
